7c36f59123caed72a06195e760101d7f895ddf38
kirubeltadesse <kirubel.m.tadesse@gmail.com>
2019-08-15 16:10:35 -0400

    fix the outlier function and the cleanup function

--- script/cleanup.py
+++ script/cleanup.py
@@ -4,11 +4,15 @@ def cleanup(df, onehot=True):
     # droping the unknow values
     df = df.dropna()
 
-    # Putting the index as a column
-    df = df.reset_index()
+    if('index' not in df.columns):
+        df = df.reset_index()
+
     #     df['Year'] = pd.DatetimeIndex(df['index']).year
     df['Month'] = pd.DatetimeIndex(df['index']).month
     df['Day'] = pd.DatetimeIndex(df['index']).day
+    df = df.set_index('index')
+    df = df.reset_index(drop=True)
+
 
     # The "stage" column is really categorical, not numeric. so covert that to a one-hot:
     if(onehot):
@@ -18,6 +22,6 @@ def cleanup(df, onehot=True):
         df['Veraison'] = (Stage == 3) *1.0
         df['Harvest'] = (Stage == 4)*1
 
-    df = df.drop('index',axis=1)
+    #df = df.drop('index',axis=1)
     df = df.drop('type', axis=1)
     return df
